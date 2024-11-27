7c08b124a791292d4d38b260c9c9bc9145fe7961
Yahya Mallak <yahya10mallak@gmail.com>
2024-11-27 22:56:42 +0100

    modify response class

--- src/Http/Response.php
+++ src/Http/Response.php
@@ -5,6 +5,7 @@ namespace Masar\Http;
 class Response
 {
 
+
     /**
      * Contains the content that will be sent as a response.
      * @var mixed
@@ -15,7 +16,22 @@ class Response
      * Contains the status code of the response.
      * @var int
      */
-    private int $statusCode;
+    private int $statusCode = 200;
+
+
+    /**
+     * Contains the response headers.
+     * @var array
+     */
+    private array $headers = [];
+
+
+    /**
+     * Contains the response content and some other information.
+     * @var array
+     */
+    private array $body = [];
+
 
     /**
      * Sets the response's content
@@ -40,8 +56,8 @@ class Response
      * @param int $code
      * @return static
      */
-    public function status(int $code = 200): static {
-        $this->statusCode = $code;
+    public function statusCode(int $statusCode): static {
+        $this->statusCode = $statusCode;
         return $this;
     }
 
@@ -50,18 +66,64 @@ class Response
      * Gets the response's status code
      * @return int
      */
-    public function getStatus() {
+    public function getStatusCode() {
         return $this->statusCode;
     }
 
+    /**
+     * Sets the response headers.
+     * @param string $name
+     * @param string $value
+     * @return Response
+     */
+    public function header(string $name, string $value): static {
+        $this->headers[strtolower($name)] = strtolower($value);
+        return $this;
+    }
+
+    /**
+     * Gets the response headers and set them.
+     * @return void
+     */
+    private function getHeaders(): void {
+        foreach($this->headers as $name => $value) {
+            header($name .":". $value);
+        }
+    }
+
+
+    /**
+     * Gets the response content and put it in the body.
+     * @return void
+     */
+    private function body(): void  {
+        $this->body = [
+            "statusCode"=> $this->getStatusCode(),
+            "data" => $this->getContent(),
+        ];
+    }
 
     /**
      * Sends the response.
      * @return mixed
      */
-    public function send() {
+    public function send(): mixed {
         http_response_code($this->statusCode);
+        $this->getHeaders();
         return $this->content;
+    }
+
+
+    /**
+     * Sends the response as JSON format.
+     * @return bool|string
+     */
+    public function sendAsJSON(): bool|string {
+        http_response_code($this->statusCode);
+        header("Content-type: application/json");
+        $this->getHeaders();
+        $this->body();
+        return json_encode($this->body);
     }
    
 }
